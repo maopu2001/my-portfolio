f12f7f51f390d6151700689bfa430d11a30f71fd
maopu2001 <maopu2001@gmail.com>
2024-12-21 00:44:10 +0600

    added my project card component

--- src/components/ProjectsCard.tsx
+++ src/components/ProjectsCard.tsx
@@ -1,12 +1,48 @@
 import Image from 'next/image';
 import React from 'react';
+import getIcons from '@/lib/icons';
 
-export default function ProjectsCard() {
+type ProjectInfoType = {
+  techStack: string[];
+  coverSrc: string;
+  iconSrc: string;
+  projectName: string;
+  projectDescription: string;
+};
+
+export default function ProjectsCard({ projectInfo }: { projectInfo: ProjectInfoType }) {
   return (
-    <div className="p-4 shadow-md rounded-md flex flex-col gap-2">
-      <Image src="/tic-tac-toe.png" width={50} height={50} alt="Tic-tac-toe Logo" />
-      <h1 className="text-2xl font-bold">Tic Tac Toe</h1>
-      <p className="text-lg">A simple game of tic-tac-toe</p>
+    <div className="relative aspect-[1.5] p-4 rounded-2xl overflow-hidden group *:text-black">
+      <Image
+        className="group-hover:scale-125 transform transition-transform duration-700 select-none -z-10 object-cover"
+        src={projectInfo.coverSrc}
+        fill
+        alt=""
+      />
+      <div className="absolute inset-0 bg-gradient-to-t from-black/90 to-black/40 select-none"></div>
+
+      <div className="flex flex-col gap-2 absolute bottom-10 left-10">
+        <div className="flex items-center gap-4 pb-2">
+          <Image src={projectInfo.iconSrc} width={50} height={50} alt="" />
+          <h1 className="text-2xl font-bold">{projectInfo.projectName}</h1>
+        </div>
+        <div className="flex gap-2">
+          {projectInfo.techStack.map((tech, i) => {
+            const info: [React.JSX.Element, string] | null = getIcons(tech);
+            if (!info) return null;
+            return (
+              <div
+                className="border p-1 rounded-md bg-black/30 flex gap-2 justify-center items-center"
+                key={i}
+                id={info[1]}
+              >
+                {info[0]} {info[1]}
+              </div>
+            );
+          })}
+        </div>
+        <p className="text-lg">{projectInfo.projectDescription}</p>
+      </div>
     </div>
   );
 }
--- src/components/home/Projects.tsx
+++ src/components/home/Projects.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import ProjectsCard from '../ProjectsCard';
 
+const tic = {
+  techStack: ['Next.js', 'Tailwind CSS', 'JavaScript'],
+  coverSrc: '/tic-tac-toe/cover.png',
+  iconSrc: '/tic-tac-toe/tic-tac-toe.png',
+  projectName: 'Tic Tac Toe',
+  projectDescription: 'A simple game of tic-tac-toe',
+};
+
 export default function Projects() {
   return (
     <div className="flex flex-col gap-4">
       <h1 className="text-3xl font-bold">My Projects</h1>
-      <ProjectsCard />
+      <ProjectsCard projectInfo={tic} />
     </div>
   );
 }
